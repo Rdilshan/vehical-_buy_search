05dd84af39f21d54ea6323a69718bb49f6aa10b2
Randika Dilshan <rdilshan077788@gmail.com>
2024-04-13 22:03:10 +0530

    post method craete

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,18 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 @app.route('/',methods=['GET'])
 def user():
-        return jsonify({"message":"Hello World"})
+        return jsonify({"message":"working....."})
     
-    
+@app.route("/search", methods=["POST"])
+def search():
+
+    if request.data:
+        data = request.data.decode("utf-8")  
+        json_data = request.get_json()
+        return jsonify({"received_data": json_data}), 200
+    else:
+        return jsonify({"message": "No data received"}), 400
     
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+        app.run(debug=True,port=8080)
