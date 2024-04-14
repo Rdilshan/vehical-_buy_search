1d0b3d38ce07aca49e13a78b376e839d8fe1a556
Randika Dilshan <rdilshan077788@gmail.com>
2024-04-14 09:40:33 +0530

    add sample

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,6 @@
+
 from flask import Flask,jsonify,request
+import requests
 app = Flask(__name__)
 @app.route('/',methods=['GET'])
 def user():
@@ -11,9 +13,17 @@ def search():
         data = request.data.decode("utf-8")  
         json_data = request.get_json()
         id_value = json_data.get("id")
+        ikmam()
         return jsonify({"received_data": json_data,"id":id_value}), 200
     else:
         return jsonify({"message": "No data received"}), 400
     
+
+    
 if __name__ == '__main__':
         app.run(debug=True)
+        
+        
+def ikmam():
+    r = requests.get("https://ikman.lk/en/ads?query=car")
+    print(r.status_code)
